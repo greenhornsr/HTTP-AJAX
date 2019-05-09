ff740ff08666ce2e3603c933725abcbe41fc6c25
greenhornsr <srollins11279@gmail.com>
2019-05-09 16:46:39 -0700

    adjusted FriendForm inputs to map properly when enabling alternate inputs with unicode magnifying glass

--- friends/src/components/FriendForm.jsx
+++ friends/src/components/FriendForm.jsx
@@ -7,13 +7,13 @@ const FriendForm = props => {
                 <h1>Add a New Friend</h1>
                 <form onSubmit={props.postFriend}>
                     <label htmlFor="name">Name: </label>
-                    {/* <input onChange={this.handleChanges} type="text" value={this.state.friend.name} name="name" placeholder={`\uD83D\uDD0E`} /> */}
+                    {/* <input onChange={props.handleChanges} type="text" value={props.friend.name} name="name" placeholder={`\uD83D\uDD0E`} /> */}
                     <input onChange={props.handleChanges} type="text" value={props.friend.name} name="name" />
                     <label htmlFor="age">Age: </label>
-                    {/* <input onChange={this.handleChanges} type="number" value={this.state.friend.age} name="age" placeholder={`\uD83D\uDD0E`} /> */}
+                    {/* <input onChange={props.handleChanges} type="number" value={props.friend.age} name="age" placeholder={`\uD83D\uDD0E`} /> */}
                     <input onChange={props.handleChanges} type="number" value={props.friend.age} name="age" />
                     <label htmlFor="email">Email: </label>
-                    {/* <input onChange={this.handleChanges} type="text" value={this.state.friend.email} name="email" placeholder={`\uD83D\uDD0E`} /> */}
+                    {/* <input onChange={props.handleChanges} type="text" value={props.friend.email} name="email" placeholder={`\uD83D\uDD0E`} /> */}
                     <input onChange={props.handleChanges} type="text" value={props.friend.email} name="email" />
                     <button className="addF-Button" type="submit">Add a Friend</button>
                     <div className="textarea-div">
